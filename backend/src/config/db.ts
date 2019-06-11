9c40d6de649fe74c12390f49814cd94baeac9df2
Emil Lozev <emilfrogy@gmail.com>
2019-06-11 18:58:51 +0100

    Added file to handle db connection

---
+++ backend/src/config/db.ts
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+
+const connection = new Sequelize('birdietest', 'test-read', 'xnxPp6QfZbCYkY8', {
+    host: 'birdie-test.cyosireearno.eu-west-2.rds.amazonaws.com',
+    dialect: 'mysql',
+    port: 3306
+});
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Successful connection to the DB!");
+    })
+    .catch((err: any) => {
+        console.log("Error connecting to the DB: ", err);
+    });
+
+
+interface dbInstance {
+    Sequelize: any,
+    instance: any
+}
+
+const db : dbInstance = {
+    Sequelize: Sequelize,
+    instance: connection
+};
+//TODO: put event instance in the db
+module.exports = db;
