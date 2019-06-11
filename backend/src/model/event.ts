c2316bd483b63f84a4ab87c4822aa59748b2c648
Emil Lozev <emilfrogy@gmail.com>
2019-06-11 19:23:14 +0100

    Added model folder plus model for events

---
+++ backend/src/model/event.ts
@@ -0,0 +1,22 @@
+module.exports = (Sequelize: any, instance: any) => {
+
+    const Event = instance.define('events', {
+        payload: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+            default: null
+        },
+        alert_id: Sequelize.UUID,
+        task_instance: Sequelize.STRING(255),
+        visit_id: Sequelize.UUID,
+        caregiver_id: Sequelize.UUID,
+        payload_as_text: Sequelize.TEXT,
+        rejected_event_id: Sequelize.STRING(255),
+        observation_event_id: Sequelize.STRING(255),
+        timestamp: Sequelize.STRING(50),
+        id: Sequelize.UUID,
+        event_type: Sequelize.STRING(50),
+        care_recipient_id: Sequelize.UUID
+    });
+    return Event;
+}
