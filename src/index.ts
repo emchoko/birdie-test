3a2864c67a87281e3a8a095d7154d8c85d033e0c
Emil Lozev <emilfrogy@gmail.com>
2019-06-13 14:05:23 +0100

    Unused blob removed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import app from './application';
 const port = process.env.PORT || 8000;
 
-declare interface XMLHttpRequest {}
-declare interface Blob {}
-
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`);
