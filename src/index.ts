3a66ac38c9a2ebe5ca0c02449a280d2a46263cea
Emil Lozev <emilfrogy@gmail.com>
2019-06-13 13:58:34 +0100

    Work around ts errors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import app from './application';
 const port = process.env.PORT || 8000;
 
+declare interface XMLHttpRequest {}
+declare interface Blob {}
+
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`);
